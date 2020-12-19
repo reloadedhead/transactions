6198aa347280050d9a4a713007f82b8af7a26335
Tomás García Gobet <tomas.garcia@dxc.com>
2020-12-19 18:33:48 -0500

    add: suspense for lazy loading

--- src/components/app-shell/content/index.tsx
+++ src/components/app-shell/content/index.tsx
@@ -1,10 +1,17 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { CircularProgress } from '@material-ui/core';
+import { lazy, Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const ContentRouter = () => (
   <Switch>
     <Route path="/inout" component={() => <div>Hello World!</div>} />
     <Redirect to="/inout" />
   </Switch>
+  <Suspense fallback={() => <CircularProgress size={30} />}>
+    <Switch>
+      <Redirect to="/inout" />
+    </Switch>
+  </Suspense>
 );
 
 export default ContentRouter;
