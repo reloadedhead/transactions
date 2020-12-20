ef404228d910f0aac91d5a223c6a2f2a97a51001
Tomás García Gobet <tomas.garcia@dxc.com>
2020-12-20 13:45:19 -0500

    add: transaction list controls

--- src/components/transactions/index.tsx
+++ src/components/transactions/index.tsx
@@ -1,7 +1,8 @@
-import { createStyles, Divider, List, makeStyles, Theme } from '@material-ui/core';
-import React from 'react';
-import { Transaction } from '../../types';
+import React, { useState } from 'react';
+import { Button, createStyles, Divider, List, makeStyles, Theme } from '@material-ui/core';
+import { Transaction, TransactionType } from '../../types';
 import TransactionListItem from './item';
+import { DropdownIcon } from '../icons';
 
 const mockTransactions: Transaction[] = [
   {
@@ -51,24 +52,77 @@ const useStyles = makeStyles((theme: Theme) =>
     list: {
       backgroundColor: theme.palette.background.paper,
       borderRadius: 8,
-      marginRight: 24,
-      marginLeft: 24,
       marginTop: 8,
+    },
+    root: {
+      paddingLeft: 24,
+      paddingRight: 24,
+    },
+    listOptions: {
+      marginBottom: 8,
+      marginTop: 24,
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    selectButtonLabel: {
+      textTransform: 'capitalize',
+    },
+    grow: {
+      flexGrow: 1,
     },
   })
 );
 
 const Transactions = () => {
   const classes = useStyles();
+  const [viewingType, setViewingType] = useState<TransactionType>('expense');
+
+  const handleSelectViewingType = (selectedViewingType: TransactionType) => {
+    setViewingType(selectedViewingType);
+  };
+
   return (
-    <List className={classes.list}>
-      {mockTransactions.map((t, i) => (
-        <>
-          <TransactionListItem transaction={t} />
-          {i < mockTransactions.length - 1 && <Divider variant="middle" component="li" />}
-        </>
-      ))}
-    </List>
+    <div className={classes.root}>
+      <div className={classes.listOptions}>
+        <Button
+          onClick={() => handleSelectViewingType('expense')}
+          classes={{ label: classes.selectButtonLabel }}
+          size="small"
+          variant="text"
+          color={viewingType === 'expense' ? 'primary' : 'default'}
+        >
+          Expenses
+        </Button>
+        <Button
+          onClick={() => handleSelectViewingType('earning')}
+          classes={{ label: classes.selectButtonLabel }}
+          size="small"
+          variant="text"
+          color={viewingType === 'earning' ? 'primary' : 'default'}
+        >
+          Earnings
+        </Button>
+        <div className={classes.grow} />
+        <Button
+          classes={{ label: classes.selectButtonLabel }}
+          size="small"
+          variant="text"
+          endIcon={<DropdownIcon height={16} width={16} fill="fff" />}
+        >
+          Sort By
+        </Button>
+      </div>
+      <List className={classes.list}>
+        {mockTransactions
+          .filter((t) => t.type === viewingType)
+          .map((t, i) => (
+            <>
+              <TransactionListItem transaction={t} />
+              {i < mockTransactions.length - 1 && <Divider variant="middle" component="li" />}
+            </>
+          ))}
+      </List>
+    </div>
   );
 };
 
