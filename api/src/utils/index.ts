72bc811dc781aae37a041e3f8f1bd9378f04e459
Tomás García Gobet <tomas.garcia@dxc.com>
2020-12-25 15:55:19 -0500

    fix: typings

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,6 +1,6 @@
 const regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d+)(Z|([-+])(\d{2}):(\d{2}))?)?)?)?$/;
 
-export default (key, value) => {
+export default (key: any, value: any) => {
   let match;
   if (typeof value === 'string' && (match = value.match(regexIso8601))) {
     const milliseconds = Date.parse(match[0]);
